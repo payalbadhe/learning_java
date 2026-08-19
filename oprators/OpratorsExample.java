6a82cc94425ef925310528159797be49b7aa8bf2
payalbadhe <payalbadhe2004@gmail.com>
2026-08-19 09:57:19 +0530

    oprators

---
+++ oprators/OpratorsExample.java
@@ -0,0 +1,31 @@
+public class OpratorsExample{
+    public static void main(String[] args) {
+        //Arithmatic oprators
+        int num1 =  10;
+        int num2 = 5;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+
+        //Logical Oprators
+        boolean a = true;
+        boolean b = true;
+
+        boolean isAdmin = false;
+        System.out.println(!isAdmin);
+
+        //unary
+        int num = 7;
+        int result = num++;
+        System.out.println(result); //7
+        System.out.println(num); //8
+
+        result = ++num;
+        System.out.println(result);//9
+        System.out.println(num);
+
+    }
+}
